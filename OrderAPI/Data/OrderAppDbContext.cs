95a8e13ecd0f1f89d6608ca06503429a936f8238
Naresh Kumar Rai <n.rai@student.fontys.nl>
2023-05-24 04:51:18 +0200

    Established Database Connection
    Created Models, Installed Nuget Packages

---
+++ OrderAPI/Data/OrderAppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderAPI.Models;
+
+namespace OrderAPI.Data
+{
+    public class OrderAppDbContext: DbContext
+    {
+        public OrderAppDbContext(DbContextOptions<OrderAppDbContext> options) : base(options) { }
+
+        public DbSet<OrderHeader> OrderHeaders { get; set; }
+        public DbSet<OrderDetails> OrderDetails { get; set; }
+
+    }
+}
